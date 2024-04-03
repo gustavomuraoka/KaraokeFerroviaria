be3ab177fb29ba377752cee72ab8763744779199
ericloumendes <77221715+ericloumendes@users.noreply.github.com>
2024-04-03 09:57:12 -0300

    Added a not found message

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 import unidecode
 from flask import Flask, render_template, request  #Importa a classe flask
 
@@ -14,6 +13,7 @@ def home():  #Função para retornar uma mensagem
     if request.method == 'POST':
         pesquisa_input = request.form['musica']
         musicas_pesquisa = requestMusic(pesquisa_input)
+        if len(musicas_pesquisa) == 0: musicas_pesquisa.append("Música não encontrada")
         return render_template("index.html", len = len(musicas_pesquisa), musicas_Ids = musicas_pesquisa)
 
     return render_template("index.html", len = len(musicas_Ids), musicas_Ids = musicas_Ids)
